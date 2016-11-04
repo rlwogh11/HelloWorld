2fe6d3622238393f3ce63a8c7f6b5f6297cd7cab
jaho <rlwogh11@naver.com>
2016-11-04 10:15:36 +0900

    master 수정

--- HelloWorld/src/com/usnschool/HelloWorld.java
+++ HelloWorld/src/com/usnschool/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Git Test~!!!");
 		System.out.println("hotfix edited~!!!");
+		System.out.println("master~!!!!");
 	}
 
 }
